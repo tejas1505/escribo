91a4c9f48cc6a0ed22f31dc30039b9b3ca123ab2
tejas1505 <tejaschavan0501@gmail.com>
2025-09-06 19:01:37 +0530

    Vercel file pushed

--- backend/src/utils/executeQuery.ts
+++ backend/src/utils/executeQuery.ts
@@ -1,15 +1,12 @@
 import pool from '../db/pg.js'
 
 const executeQuery = async (query: string, values?: any[]) => {
-    const client = await pool.connect();
     try {
-        const result = await client.query(query, values);
+        const result = await pool.query(query, values);
         return result;
     } catch (error) {
         console.error('Error executing query:', error);
         throw error;
-    } finally {
-        client.release();
     }
 }
 
